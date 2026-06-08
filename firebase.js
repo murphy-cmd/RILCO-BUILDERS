18fd43c4d0d4ba35d5af39f1f951052c9bde52d2
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-06-08 15:32:40 +0800

    Add Firebase configuration to firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFntM-PJLlZdc-bX7YPQNWz584LXeFrvI",
+  authDomain: "rilco-builders-website.firebaseapp.com",
+  projectId: "rilco-builders-website",
+  storageBucket: "rilco-builders-website.firebasestorage.app",
+  messagingSenderId: "826092464882",
+  appId: "1:826092464882:web:788b88436fd4440b373e6b",
+  measurementId: "G-X4WQ5WQTH7"
+};
