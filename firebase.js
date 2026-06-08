cfa7b1fba52f086b36fbb4bca1b0ac594c088ed3
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-06-08 16:41:18 +0800

    Update Firebase configuration with new project details

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDFntM-PJLlZdc-bX7YPQNWz584LXeFrvI",
+  authDomain: "rilco-builders-website.firebaseapp.com",
+  projectId: "rilco-builders-website",
+  storageBucket: "rilco-builders-website.firebasestorage.app",
+  messagingSenderId: "826092464882",
+  appId: "1:826092464882:web:788b88436fd4440b373e6b"
 };
 
 // Initialize Firebase
