9e0842cfd79b02415421d6793d7c6b29b9150d15
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-06-08 18:19:42 +0800

    Add Firestore initialization to firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDFntM-PJLlZdc-bX7YPQNWz584LXeFrvI",
@@ -15,7 +15,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Authentication
+// Services
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, auth };
+// Export
+export { app, auth, db };
