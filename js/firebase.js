6243fb6867e67cc9a3d9f78d1640c6fe4e2138cd
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-06-08 18:39:28 +0800

    Initialize Firebase app with authentication and Firestore

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyDFntM-PJLlZdc-bX7YPQNWz584LXeFrvI",
+authDomain: "rilco-builders-website.firebaseapp.com",
+projectId: "rilco-builders-website",
+storageBucket: "rilco-builders-website.firebasestorage.app",
+messagingSenderId: "826092464882",
+appId: "1:826092464882:web:788b88436fd4440b373e6b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
