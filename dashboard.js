df7fe4f52d4675fef383578e48ec506738f64d90
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-06-08 18:02:21 +0800

    Integrate Firestore to display user welcome message
    Added Firestore integration to fetch user data on auth state change.

--- dashboard.js
+++ dashboard.js
@@ -1,11 +1,44 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
+
 import {
-  signOut
+onAuthStateChanged,
+signOut
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-window.logout = function () {
-  signOut(auth).then(() => {
-    alert("Logged out!");
-    window.location.href = "login.html";
-  });
+import {
+doc,
+getDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+onAuthStateChanged(auth, async(user)=>{
+
+if(user){
+
+const docRef =
+doc(db,"users",user.uid);
+
+const docSnap =
+await getDoc(docRef);
+
+if(docSnap.exists()){
+
+document.getElementById("welcome").innerHTML =
+`Welcome, ${docSnap.data().fullname}`;
+
+}
+
+}else{
+
+window.location.href="login.html";
+
+}
+
+});
+
+window.logout = async function(){
+
+await signOut(auth);
+
+window.location.href="login.html";
+
 };
