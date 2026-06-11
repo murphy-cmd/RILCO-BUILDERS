18dcab7616392f230c0423bdc145580211c1c8e1
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-06-11 18:07:47 +0800

    Fix Firebase imports and improve alert messages
    Updated Firebase import paths and modified alert messages for success and error handling.

--- js/contact-admin.js
+++ js/contact-admin.js
@@ -1,10 +1,10 @@
-import { db } from "./firebase-config.js";
+import { db } from "./firebase.js";
 
 import {
   collection,
   addDoc,
   serverTimestamp
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const form = document.getElementById("contactForm");
 
@@ -16,6 +16,7 @@ form.addEventListener("submit", async (e) => {
     const message = document.getElementById("message").value;
 
     try {
+
         await addDoc(collection(db, "messages"), {
             name,
             email,
@@ -23,11 +24,13 @@ form.addEventListener("submit", async (e) => {
             createdAt: serverTimestamp()
         });
 
-        alert("Message Sent!");
+        alert("Message Sent Successfully!");
         form.reset();
 
     } catch (error) {
-        console.error(error);
-        alert("Error sending message.");
+
+        console.error("Firebase Error:", error);
+        alert("Failed to send message.");
+
     }
 });
